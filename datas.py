59cb810553993baf22211a405567eb8a26d53a1f
tonytan748 <tonytan748@gmail.com>
2014-05-04 18:59:20 +0800

    invoice system

---
+++ datas.py
@@ -0,0 +1,134 @@
+#-*-coding=utf-8-*-
+import os
+import sqlite3
+import datetime
+import sys
+
+FILEPATH=os.path.join(os.path.split(sys.argv[0])[0],'data')
+FILENAME=os.path.join(FILEPATH,'invoice.db')
+
+USER=os.path.join(FILEPATH,'user.txt')
+
+def mainprocess(content):
+	try:
+		db=sqlite3.connect(FILENAME)
+		cursor=db.cursor()
+		cursor.execute(content)
+		db.commit()
+	except Exception as e:
+		db.rollback()
+		print e
+	except DatabaseError as e:
+		db.rollback()
+		raise e
+	finally:
+		db.close()
+
+def createTable():
+	content='''
+		CREATE TABLE invoice(
+			id INTEGER PRIMARY KEY NOT NULL,
+			invoice_no TEXT NOT NULL,
+			invoice_rev TEXT NOT NULL,
+			invoice_date TEXT NOT NULL,
+			project_code TEXT NOT NULL,
+			project_title TEXT NOT NULL,
+			client_company_name TEXT NOT NULL,
+			client_name TEXT NOT NULL,
+			quotation_date TEXT NOT NULL,
+			quotation_ref TEXT NOT NULL,
+			client_po_no TEXT NOT NULL,
+			credit_note_no TEXT NOT NULL,
+			credit_note_rev TEXT NOT NULL,
+			invoice_amount TEXT NOT NULL,
+			cn_amount TEXT NOT NULL,
+			retention_persent TEXT NOT NULL,
+			retention_acount TEXT NOT NULL,
+			gross_amount TEXT NOT NULL,
+			gst TEXT NOT NULL,
+			net_invoice_amount TEXT NOT NULL,
+			remarks TEXT NOT NULL,
+			create_by TEXT NOT NULL,
+			create_date TEXT NOT NULL,
+			is_delete TEXT NOT NULL
+			delete_by TEXT NOT NULL
+			delete_date TEXT NOT NULL
+		)
+	'''
+	mainprocess(content)
+	
+def getData():
+	try:
+		db=sqlite3.connect(FILENAME)
+		c=db.cursor()
+		c.execute('SELETEC * FROM invoice')
+		x=[]
+		for i in c:
+			if not i[23]:
+				m={'id':{0},'invoice_no':{1},'invoice_rev':{2},'invoice_date':{3},	'project_code':{4},'project_title':{5},'client_company_name':{6},'client_name':{7},'quotation_date':{8},'quotation_ref':{9},'client_po_no':{10},'credit_note_no':{11},'credit_note_rev':{12},'invoice_amount':{13},'cn_amount':{14},'retention_persent':{15},'retention_acount':{16},'gross_amount':{17},'gst':{18},'net_invoice_amount':{19},'remarks':{20},'create_by':{21},'create_date':{22}}
+				x.append(m)
+		db.commit()
+		return x
+	except Exception as e:
+		db.rollback()
+		print e
+	except DatabaseError as e:
+		db.rollback()
+		raise e
+	finally:
+		db.close()
+
+def searchInv(inv_no):
+	if inv_no:
+		m=getData()
+		x=(for i in m if inv_no in i['invoice_no'])
+		if x:
+			return list(x)
+		else:
+			return
+
+def searchProject(pro_code):
+	if inv_no:
+		m=getData()
+		x=(for i in m if pro_code in i['project_code'])
+		if x:
+			return list(x)
+		else:
+			return
+
+def add(inv_list):
+	if inv_list:
+		content='''
+			INSERT INTO invoice(invoice_no,invoice_rev,invoice_date,project_code,project_title,client_company_name,client_name,quotation_date,quotation_ref,client_po_no,credit_note_no,credit_note_rev,invoice_amount,cn_amount,retention_persent,retention_acount,gross_amount,gst,net_invoice_amount,remarks,create_by,create_date) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)
+		''',(inv_list)
+		mainprocess(content)
+		return True
+
+def update(inv_id,inv_list):
+	if inv_id and inv_list:
+		content='''
+			UPDATE invoice SET invoice_no=?, invoice_rev=?, invoice_date=?, project_code=?, project_title=?, client_company_name=?, client_name=?, quotation_date=?, quotation_ref=?, client_po_no=?, credit_note_no=?, credit_note_rev=?, invoice_amount=?, cn_amount=?, retention_persent=?, retention_acount=?, gross_amount=?, gst=?, net_invoice_amount=?, remarks=?, create_by=?, create_date=? WHERE id=?
+		''',(','.join(inv_list),inv_id)
+		mainprocess(content)
+		return True
+		
+def delete(inv_id,del_info):
+	if inv_id and del_info:
+		content='''
+			UPDATE invoice SET is_delete=1, delete_by=?, delete_date=?
+		''',(inv_id,','.join(del_info))
+		mainprocess(content)
+		return True
+
+
+#==========LOGIN IN===========
+def getName():
+	a=[]
+	with open(USER,'r') as f:
+		for i in f.readlines():
+			m={}
+			x=i.split(',')
+			m['username']=x[0]
+			m['password']=x[1]
+			a.append(m)
+	return a
---
+++ main.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python
+#coding=utf-8
+
+
+from smtplib import *
+from Tkinter import *
+import tkMessageBox
+import string
+
+import datas
+
+FILEPATH=os.path.join(os.path.split(sys.argv[0])[0],'data')
+FILENAME=os.path.join(FILEPATH,'invoice.db')
+
+USER=os.path.join(FILEPATH,'user.txt')
+
+
+class loginPage(object):
+
+    def __init__(self, master, info='Invoice Management'):
+        self.master = master
+        self.mainlabel = Label(master, text=info, justify=CENTER)
+        self.mainlabel.grid(row=0, columnspan=3)
+
+
+        self.user = Label(master, text='username', borderwidth=2)
+        self.user.grid(row=1, sticky=W)
+
+
+        self.pwd = Label(master, text='password', borderwidth=2)
+        self.pwd.grid(row=2, sticky=W)
+		
+		att=datas.getName()
+		attn=[i['username'] for i in att]
+		self.userEntry=ttk.Combobox(master)
+		self.userEntry['values']=tuple(attn)
+		self.userEntry.grid(row=1,column=1,columnspan=2)
+		self.userEntry.focus_set()
+		
+
+        self.pwdEntry = Entry(master, show='*')
+        self.pwdEntry.grid(row=2, column=1, columnspan=2)
+
+
+        self.loginButton = Button(master, text='Login', borderwidth=2, command=self.login)
+        self.loginButton.grid(row=3, column=1)
+
+
+        self.clearButton = Button(master, text='Clear', borderwidth=2, command=self.clear)
+        self.clearButton.grid(row=3, column=2)
+
+
+    def login(self):
+        self.username = (self.userEntry.get().strip()).upper()
+        self.passwd = self.pwdEntry.get().strip()
+		a=datas.getName()
+		attn=[i['username'] for i in a]
+		if len(self.username) == 0 or len(self.passwd) == 0 or not self.username in attn:
+			tkMessageBox.showwarning('please check your username')
+			self.clear()
+			self.userEntry.focus_set()
+            return
+		else:
+			pw=[i['password'] for i in a if i['username'] == self.username]
+			if not self.passwd == pw[0]:
+				tkMessageBox.showwarning('please check your username')
+				self.clear()
+				self.userEntry.focus_set()
+				return
+				
+        self.connect()
+
+
+    def connect(self):
+		self.username = (self.userEntry.get().strip()).upper()
+        self.invoice = invoice(self.master,self.username)
+
+
+    def clear(self):
+        self.userEntry.delete(0, END)
+        self.pwdEntry.delete(0, END)
+
+class invoice(object):
+
+    def __init__(self, master, user=''):
+        self.user = user
+
+        self.sp = Toplevel(master)
+
+
+        self.sendToLabel = Label(self.sp, text='send to:')
+        self.sendToLabel.grid()
+        self.sendToEntry = Entry(self.sp)
+        self.sendToEntry.grid(row=0, column=1)
+
+
+        self.subjectLabel = Label(self.sp, text='subject:')
+        self.subjectLabel.grid(row=1, column=0)
+        self.subjectEntry = Entry(self.sp)
+        self.subjectEntry.grid(row=1, column=1)
+
+
+        self.fromToLabel = Label(self.sp, text='from to:')
+        self.fromToLabel.grid(row=2, column=0)
+        self.formToAdd = Label(self.sp, text=self.sender)
+        self.formToAdd.grid(row=2, column=1)
+
+
+        self.sendText = Text(self.sp)
+        self.sendText.grid(row=3, column=0, columnspan=2)
+
+
+        self.sendButton = Button(self.sp, text='send', command=self.sendMail)
+        self.sendButton.grid(row=4, column=0)
+
+
+        self.newButton = Button(self.sp, text='new mail', command=self.newMail)
+        self.newButton.grid(row=4, column=1)
+
+
+    def getMailInfo(self):
+        self.sendToAdd = self.sendToEntry.get().strip()
+        self.subjectInfo = self.subjectEntry.get().strip()
+        self.sendTextInfo = self.sendText.get(1.0, END)
+
+
+    def sendMail(self):
+        self.getMailInfo()
+        body = string.join(("From: %s" % self.sender, "To: %s" % self.sendToAdd, "Subject: %s" % self.subjectInfo, "", self.sendTextInfo), "\r\n")
+        try:
+            self.smtp.sendmail(self.sender, [self.sendToAdd], body)
+        except Exception, e:
+            tkMessageBox.showerr('发送失败', "%s" % e)
+            return
+        tkMessageBox.showinfo('提示', '邮件已发送成功！')
+
+
+    def newMail(self):
+        self.sendToEntry.delete(0, END)
+        self.subjectEntry.delete(0, END)
+        self.sendText.delete(1.0, END)
+
+
+if __name__ == '__main__':
+
+
+    root = Tk()
+    root.title('简易发送邮件程序')
+
+
+    myLogin = loginPage(root)
+
+
+    #root.wait_window(myLogin.mySendMail.sp)
+    mainloop()
